a6494db017f6aeaaa40ecfa828263bebc22b627a
brycesilan <brycesilan@gmail.com>
2018-06-27 06:39:10 -0700

    jQuery?

--- js/snake.js
+++ js/snake.js
@@ -999,3 +999,4 @@ getHighScore();
           funcIntervalQuery();
       }, 10000); //it's recommended that the query frequency is between 10-15s.
   </script>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
