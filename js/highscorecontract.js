2386e1bb530ee1e7decda322bab7a0fcc0ae0d29
brycesilan <brycesilan@gmail.com>
2018-06-27 08:22:28 -0700

    Possibly?

--- js/highscorecontract.js
+++ js/highscorecontract.js
@@ -8,23 +8,11 @@ Highscore.prototype = {
     },
     set: function (name, value) {
         // Storing a string
-        LocalContractStorage.set("name",name);
-        // Storing a number (value)
         LocalContractStorage.set("value", value);
-        // Storing an objects
-        LocalContractStorage.set("obj", {name:name, value:value});
     },
     get: function () {
-        var name = LocalContractStorage.get("name");
-        console.log("name:" + name)
         var value = LocalContractStorage.get("value");
-        console.log("value:" + value)
-        var obj = LocalContractStorage.get("obj");
-        console.log("obj:" + JSON.stringify(obj))
+        console.log("highscore:" + value)
     },
-    del: function () {
-        var result = LocalContractStorage.del("name");
-        console.log("del result:" + result)
-    }
 };
 module.exports = Highscore;
